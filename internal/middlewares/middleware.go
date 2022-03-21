a3b53c23817958440815660166efde9d58f5a6f7
Kien Dinh <kienmatu1@gmail.com>
2022-03-21 17:30:54 +0700

    Add jwt middleware

--- internal/middlewares/middleware.go
+++ internal/middlewares/middleware.go
@@ -1,19 +1,16 @@
 package middlewares
 
 import (
-	"kienmatu/go-todos/config"
 	"kienmatu/go-todos/internal/auth"
-
-	"github.com/sirupsen/logrus"
 )
 
 type MiddlewareManager struct {
 	authUC auth.UseCase
-	cfg    *config.Configuration
-	logger *logrus.Logger
+	// cfg    *config.Configuration
+	// logger *logrus.Logger
 	// origins []string
 }
 
-func NewMiddlewareManager(authUC auth.UseCase, cfg *config.Configuration, logger *logrus.Logger) *MiddlewareManager {
-	return &MiddlewareManager{authUC, cfg, logger}
+func NewMiddlewareManager(authUC auth.UseCase) *MiddlewareManager {
+	return &MiddlewareManager{authUC}
 }
